bd25c10d2ab7f3a3d191d0b1195b632a3966b820
Pheanoukma <103352870+Pheanoukma@users.noreply.github.com>
2022-09-07 12:56:00 +0700

    Create bruh.py

---
+++ bruh.py
@@ -0,0 +1,28 @@
+import random
+from colorama import Fore, Back, Style
+
+banner = """
+------------------------
+Simple no father program
+( built for amrith)
+
+
+Check out my github :
+    github.com/pheanoukma
+
+------------------------
+"""
+print(banner)
+print(Fore.RED+"This is a test program for pro ppl like me B) ")
+
+Black = [ " Your hairline is bigger than the entire world", " ,Your dad left you", " ,I heared u got no bitches", " ,Your mother wishes to abort you", " ,Ya dick smoller than ur mom's"]
+bruh = input("")
+print(random.choice(bruh)+Black)
+if option == 69:
+    print("you've cracked the code, Good luck for you!!")
+    banner = """
+    
+    This is the prize : your mother nigger
+    
+    """
+    printer(banner)
